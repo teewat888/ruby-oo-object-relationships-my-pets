5eb5665ebbde0f9090bd82331772b2c249af0c91
teerawat prarom <teewat@yahoo.com>
2021-12-01 07:22:57 +1100

    refactored

--- lib/owner.rb
+++ lib/owner.rb
@@ -26,7 +26,7 @@ class Owner
   end
 
   def self.reset_all
-    @@all = []
+    self.all.clear
   end
 
   def cats
@@ -38,45 +38,31 @@ class Owner
   end
 
   def buy_cat(name)
-    cat = Cat.new(name,self)
+    Cat.new(name,self)
   end
 
   def buy_dog(name)
-    dog = Dog.new(name,self)
+    Dog.new(name,self)
   end
 
   def walk_dogs
-    Dog.all.map do |dog|
-      if dog.owner == self
-        dog.mood = "happy"
-      end
-    end
+    self.dogs.map {|dog| dog.mood = "happy"}
   end
 
   def feed_cats
-    Cat.all.map {|cat| cat.owner == self ? cat.mood = "happy" : cat.mood = "nervous"}
+    self.cats.map {|cat| cat.mood = "happy"}
   end
 
   def sell_pets
-    Cat.all.map do |cat|
-      if cat.owner == self
-        cat.mood = "nervous"
-        cat.owner = nil
-      end
-    end
-
-    Dog.all.map do |dog|
-      if dog.owner == self
-        dog.mood = "nervous"
-        dog.owner = nil
-      end
+    pets = self.cats + self.dogs
+    pets.map do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
     end
   end
 
   def list_pets
-    cats = Cat.all.select {|cat| cat.owner == self}.count
-    dogs = Dog.all.select {|dog| dog.owner == self}.count
-    "I have #{dogs} dog(s), and #{cats} cat(s)."
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
 
 end
